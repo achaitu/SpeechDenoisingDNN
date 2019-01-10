b80cc2b73bfc0c2314d3d4d7401dbe64748e8882
achaitu <amballachaitanya777@gmail.com>
2019-01-10 15:24:15 +0530

    adding the necessary codes

---
+++ codes/babble/cnn1000_withBN.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+__author__ = "Chaitanya"
+
+#import all the dependencies
+
+import tensorflow as tf
+with tf.device('/device:GPU:2'):
+    import keras
+    from keras.models import Sequential
+    #from keras.layers import Dense, Dropout, Flatten
+    from keras.layers import Conv2D, MaxPooling2D, Conv2DTranspose,Activation
+    from keras.layers.normalization import BatchNormalization
+    from keras import backend as K
+    import pickle as pk
+    batch_size = 1024
+    epochs = 1000
+    import numpy as  np
+    import os
+    cwd = os.getcwd()
+    #x_train = os.path.join(cwd,'Noisy_TCDTIMIT/Babble/20/volunteers/01M/straightcam')
+    #y_train = os.path.join(cwd,'Clean/volunteers/01M/straightcam')
+
+    pickle_train = open("noiseBabble_xtrain.pickle","rb")
+    x_train = pk.load(pickle_train)
+    pickle_trainlabel = open("cleanBabble.pickle","rb")
+    y_train = pk.load(pickle_trainlabel)
+
+    print("Got the input data")
+    x_train = np.asarray(x_train)
+    y_train = np.asarray(y_train)
+    x_train = x_train.reshape(x_train.shape[0], 129, 16, 1)
+    y_train = y_train.reshape(y_train.shape[0], 129, 16, 1)
+    print(np.asarray(x_train).shape)
+
+    model = Sequential()
+    #model.add(BatchNormalization())
+    model.add(Conv2D(64, kernel_size=(7, 7),padding='valid',use_bias=True, kernel_initializer='glorot_uniform',bias_initializer='zeros',input_shape=(129,16,1)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Conv2D(128, (5, 5),padding='valid',use_bias=True, kernel_initializer='glorot_uniform',bias_initializer='zeros'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Conv2D(256, (3, 3),padding='valid',use_bias=True, kernel_initializer='glorot_uniform',bias_initializer='zeros'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Conv2D(256, (1, 1),padding='valid',use_bias=True, kernel_initializer='glorot_uniform',bias_initializer='zeros'))
+    #model.add(Conv2DTranspose(128, (3, 3), activation='relu',padding='valid',use_bias=True, kernel_initializer='glorot_uniform',bias_initializer='zeros'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Conv2DTranspose(128, (3, 3),padding='valid',use_bias=True, kernel_initializer='glorot_uniform',bias_initializer='zeros'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Conv2DTranspose(64, (5, 5),padding='valid',use_bias=True, kernel_initializer='glorot_uniform',bias_initializer='zeros'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Conv2DTranspose(1, (7, 7),padding='valid',use_bias=True, kernel_initializer='glorot_uniform',bias_initializer='zeros'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    
+    #model.add(MaxPooling2D(pool_size=(2, 2)))
+    #model.add(Dropout(0.25))
+    #model.add(Flatten())
+    #model.add(Dense(128, activation='relu'))
+    #model.add(Dropout(0.5))
+    #model.add(Dense(num_classes, activation='softmax'))
+
+    model.compile(loss=keras.losses.mean_squared_error,
+                  optimizer=keras.optimizers.Adam(),
+                  metrics=['accuracy'])
+    #model.build()
+
+    print(model.summary())
+    model.fit(np.asarray(x_train), np.asarray(y_train),
+              batch_size=batch_size,
+              epochs=epochs,
+              validation_split = 0.01
+             )
+
+    model.save_weights('weights_1.h5')
+
+    #score = model.evaluate(x_train, y_train, verbose=0)
+
+    #print('Test loss:', score[0])
+    #print('Test accuracy:', score[1])
